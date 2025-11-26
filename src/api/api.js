4d42448da4339dedeefa9cc5bc02a7e7a44e913c
José Leonardo Montero Núñez <monteroleon52@gmail.com>
2025-11-26 16:42:14 -0600

    api

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,5 @@
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api'
+const API_URL = import.meta.env.VITE_API_URL || 'https://experiencias-unicas-pied.vercel.app:4000/api'
+//const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:4000/api'
 
 // Helper para obtener el token guardado en localStorage
 const getAuthHeaders = () => {
