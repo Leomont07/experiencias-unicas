256c1530327ef4ad7f0272ab27eaf57ba19eed4c
José Leonardo Montero Núñez <monteroleon52@gmail.com>
2025-11-28 17:52:14 -0600

    Update generateToken.js

--- server/src/utils/generateToken.js
+++ server/src/utils/generateToken.js
@@ -2,6 +2,6 @@ import jwt from 'jsonwebtoken'
 
 export const generateToken = (userId) => {
   return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN
+    expiresIn: '7d',
   })
 }
